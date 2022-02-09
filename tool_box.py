1da1509eb80571a95c2f1d41441ee0986972a9cd
Lavrova <lavrovaliz@gmail.com>
2022-02-09 17:05:31 +0100

    Update tool_box.py

--- tool_box.py
+++ tool_box.py
@@ -18,6 +18,8 @@ import radiomics
 from radiomics import featureextractor
 from pandas import DataFrame
 import pandas as pd
+import csv
+import cv2
 
 class tool_box(data_set):
 
@@ -187,6 +189,42 @@ class tool_box(data_set):
         else:
             raise TypeError('Currently only conversion from dicom -> nrrd is available')
 
+    def pre_process(self, ref_img_path: str = None, save_path: str = None,
+                    z_score: bool = False, norm_coeff: tuple = None, hist_match: bool = False,
+                    hist_equalize: bool = False, binning: bool = False, percentile_scaling: bool = False,
+                    subcateneus_fat: bool = False, fat_value: bool = None, verbosity: bool = False, visualize: bool = False):
+
+        ref_img_arr = sitk.GetArrayFromImage(sitk.ReadImage(ref_img_path))
+        for i, pat in tqdm(self):
+            image = sitk.ReadImage(pat[0])
+            mask = sitk.ReadImage(pat[1])
+            image_array = sitk.GetArrayFromImage(image)
+            mask_array = sitk.GetArrayFromImage(mask)
+            ref_image_arr = ref_img_arr.copy()
+            # run the pre-processing
+            pre_processed_arr = self.__preprocessing_function(image_array, mask_array, ref_image_arr)
+
+            export_dir = os.path.join(save_path, i)
+            if not os.path.exists(export_dir):
+                os.makedirs(export_dir)
+
+            pre_processed_image = sitk.GetImageFromArray(pre_processed_arr)
+            pre_processed_image.CopyInformation(image)
+            sitk.WriteImage(pre_processed_image,
+                            os.path.join(export_dir, 'pre_processed_img.nrrd'))  # save image and binary mask locally
+            sitk.WriteImage(mask, os.path.join(export_dir, 'mask.nrrd'))
+
+        params = {'ref_img_path': ref_img_path, 'save_path': save_path, 'z_score': z_score, 'norm_coeff': norm_coeff,
+                  'hist_match': hist_match, 'hist_equalize': hist_equalize, 'binning': binning,
+                  'percentile_scaling': percentile_scaling, 'subcateneus_fat': subcateneus_fat, 'fat_value': fat_value,
+                  'verbosity': verbosity, 'visualize': visualize}
+
+        with open(os.path.join(save_path, 'pre-processing_parameters.csv'), 'w') as csv_file:
+            writer = csv.writer(csv_file)
+            writer.writerow(['Pre-processing parameters:'])
+            for key, value in params.items():
+                writer.writerow([str(str(key) + ': ' + str(value))])
+
 
 
     def __get_roi_id(self,rtstruct,roi):
@@ -350,6 +388,202 @@ class tool_box(data_set):
                 return val
         except:
             return 'NaN'
+
+    def __normalize_image_zscore(self, image, norm_coeff):  ##Zscore whole image/or masked region
+        img = image - norm_coeff[0]
+        img = image / norm_coeff[1]
+        return img
+
+    def __normalize_image_zscore_per_image(self, image):  ##Zscore based on the masked region intensities
+        image_s = image.copy()
+        mu = np.mean(image_s.flatten())
+        sigma = np.std(image_s.flatten())
+        if self.verbosity:
+            print('ROI MU = %s, sigma = %s' % (mu, sigma))
+        img = (image - mu) / sigma
+        return img, mu, sigma
+
+    def __resample_intensities(self, orig_img, bin_nr):  ## Intensity resampling whole image/or masked region
+        v_count = 0
+        img_list = []
+        filtered = orig_img.copy()
+        if np.min(orig_img.flatten()) < 0:
+            filtered += np.min(orig_img.flatten())
+        resampled = np.zeros_like(filtered)
+        max_val_img = np.max(filtered.flatten())
+        step = max_val_img / bin_nr
+
+        for st in np.arange(step, max_val_img + step, step):
+            resampled[(filtered <= st) & (filtered >= st - step)] = v_count
+            v_count += 1
+        if self.verbosity:
+            print("Resampling with a step of: ", step, 'Amount of unique values, original img: ',
+                  len(np.unique(orig_img.flatten())), 'resampled img: ', len(np.unique(resampled.flatten())))
+
+        if len(np.unique(resampled.flatten())) < 255:
+            return np.array(resampled, dtype=np.uint8)
+        else:
+            return np.array(resampled, dtype=np.uint16)
+
+    def __intensity_scaling(self, img, fat_int_value=None, method=''):
+        if method == 'fat' and fat_int_value:
+            filtered = img.copy()
+            if np.min(img.flatten()) < 0:
+                filtered += np.min(img.flatten())
+            filtered = (1.0 * filtered) / (1.0 * np.max(img.flatten()))
+            filtered = 1.0 * fat_int_value * filtered
+            return np.array(filtered, np.uint16)
+
+        elif method == '95th':
+            filtered = img.copy()
+            if np.min(img.flatten()) < 0:
+                filtered += np.min(img.flatten())
+            perc_95 = np.percentile(filtered.flatten(), 95)
+            filtered = (1.0 * filtered) / (1.0 * np.max(img.flatten()))
+            filtered = 1.0 * perc_95 * filtered
+            return np.array(filtered, np.uint16)
+        else:
+            print('max value is not understood, skipping intensity rescaling step!')
+
+    def __histogram_matching(self, source, template):
+        """
+        Adjust the pixel values of a grayscale image such that its histogram
+        matches that of a target image
+
+        Arguments:
+        -----------
+            source: np.ndarray
+                Image to transform; the histogram is computed over the flattened
+                array
+            template: np.ndarray
+                Template image; can have different dimensions to source
+        Returns:
+        -----------
+            matched: np.ndarray
+                The transformed output image
+        """
+
+        oldshape = source.shape
+        source = source.ravel()
+        template = template.ravel()
+
+        # get the set of unique pixel values and their corresponding indices and
+        # counts
+        s_values, bin_idx, s_counts = np.unique(source, return_inverse=True,
+                                                return_counts=True)
+        t_values, t_counts = np.unique(template, return_counts=True)
+
+        # take the cumsum of the counts and normalize by the number of pixels to
+        # get the empirical cumulative distribution functions for the source and
+        # template images (maps pixel value --> quantile)
+        s_quantiles = np.cumsum(s_counts).astype(np.float64)
+        s_quantiles /= s_quantiles[-1]
+        t_quantiles = np.cumsum(t_counts).astype(np.float64)
+        t_quantiles /= t_quantiles[-1]
+
+        # interpolate linearly to find the pixel values in the template image
+        # that correspond most closely to the quantiles in the source image
+        interp_t_values = np.interp(s_quantiles, t_quantiles, t_values)
+
+        return interp_t_values[bin_idx].reshape(oldshape).astype(np.int16)
+
+    def __preprocessing_function(self, img, mask, ref_img, z_score, norm_coeff, hist_match, hist_equalize, binning,
+                                 percentile_scaling, subcateneus_fat, fat_value, verbosity, visualize):
+
+        if verbosity:
+            unique_number_of_intensity = np.unique(img.flatten())
+            min_int, max_int = np.min(img.flatten()), np.max(img.flatten())
+            img_type = img.dtype
+            print('-' * 40)
+            print('Original image stats:')
+            print('Number of unique intensity values :', len(unique_number_of_intensity))
+            print('Intensity values range:[%s:%s]' % (min_int, max_int))
+            print('Image intensity dtype:', img_type)
+            print('-' * 40)
+        if visualize:
+            plt.figure(figsize=(12, 12))
+            plt.imshow(img[int(len(img) / 2.), ...], cmap='bone')
+            plt.title('original image')
+            plt.show()
+
+        if subcateneus_fat:
+            img = self.__intensity_scaling(np.squeeze(img), fat_value, method='fat')
+            if verbosity:
+                print('Intensity values rescaled based on the subcateneus fat')
+            if visualize:
+                plt.figure(figsize=(12, 12))
+                plt.imshow(img[int(len(img) / 2.), ...], cmap='bone')
+                plt.title('subcateneus fat scaling')
+                plt.show()
+
+        if percentile_scaling:
+            img = self.__intensity_scaling(np.squeeze(img), method='95th')
+            if verbosity:
+                print('Intensity values rescaled based on the 95th percentile')
+            if visualize:
+                plt.figure(figsize=(12, 12))
+                plt.imshow(img[int(len(img) / 2.), ...], cmap='bone')
+                plt.title('95th percentile scaling')
+                plt.show()
+
+        if hist_match:
+            img = self.__histogram_matching(np.squeeze(img), np.squeeze(ref_img))
+            if verbosity:
+                print('Histogram matching was applied')
+            if visualize:
+                plt.figure(figsize=(22, 12))
+                plt.subplot(121)
+                plt.imshow(img[int(len(img) / 2.), ...], cmap='bone')
+                plt.title('Histogram matched')
+                plt.subplot(122)
+                plt.imshow(ref_img[int(len(img) / 2.), ...], cmap='bone')
+                plt.title('Ref image')
+                plt.show()
+
+        if binning:
+            img = self.__resample_intensities(img, binning)
+            if verbosity:
+                print('-' * 40)
+                print('Intensity values resampled with a number of bins: %s' % binning)
+                print('-' * 40)
+            if visualize:
+                plt.figure(figsize=(12, 12))
+                plt.imshow(img[int(len(img) / 2.), ...], cmap='bone')
+                plt.title('%s bins resampled' % binning)
+                plt.show()
+
+        if hist_equalize and img.dtype == np.uint8:
+            equalized = [cv2.equalizeHist(np.squeeze(x).astype(np.uint8)) for x in img]
+            img = np.stack(equalized)
+            if self.verbosity:
+                print('Histogram equalization applied')
+            if self.visualize:
+                plt.figure(figsize=(12, 12))
+                plt.imshow(img[int(len(img) / 2.), ...], cmap='bone')
+                plt.title('Histogram equalize')
+                plt.show()
+
+        if self.z_score and self.norm_coeff:
+            img = self.__normalize_image_zscore(img, self.norm_coeff)
+            if self.verbosity:
+                print('Z-score normalization applied based on the whole image, Mu=%s, sigma=%s' % (self.norm_coeff))
+            if self.visualize:
+                plt.figure(figsize=(12, 12))
+                plt.imshow(img[int(len(img) / 2.), ...], cmap='bone')
+                plt.title('Z-score normalization applied')
+                plt.show()
+
+        elif self.z_score:
+            img, mu, sigma = self.__normalize_image_zscore_per_image(img)
+            if self.verbosity:
+                print('Z-score normalization applied based on image intensities, Mu=%s, sigma=%s' % (mu, sigma))
+            if self.visualize:
+                plt.figure(figsize=(12, 12))
+                plt.imshow(img[int(len(img) / 2.), ...], cmap='bone')
+                plt.title('Z-score normalization applied')
+                plt.show()
+
+        return img
 
 
 ###Some debugs
