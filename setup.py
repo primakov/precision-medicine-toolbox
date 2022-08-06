1c38534af5f8cab3f6b81fb725e04b2949814998
Lisa Lavrova <lavrovaliz@gmail.com>
2022-08-07 00:11:01 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ long_description = (this_directory / "README.md").read_text()
 setup(
   name = 'precision-medicine-toolbox',         # How you named your package folder (MyLib)
   packages = ['pmtool'],   # Chose the same as "name"
-  version = '0.8.4',      # Start with a small number and increase it with every change you make
+  version = '0.8',      # Start with a small number and increase it with every change you make
   license='bsd-3-clause',        # Chose a license from here: https://help.github.com/articles/licensing-a-repository
   description = 'Precision medicine tool-box for medical imaging research',   # Give a short description about your library
   long_description=long_description,
@@ -15,7 +15,7 @@ setup(
   author = 'sergey primakov & lisa lavrova',                   # Type in your name
   author_email = 'primakov@bk.ru',      # Type in your E-Mail
   url = 'https://github.com/primakov/precision-medicine-toolbox',   # Provide either the link to your github or to your website
-  download_url = 'https://github.com/primakov/precision-medicine-toolbox/archive/refs/tags/0.8.4.tar.gz',    # I explain this later on
+  download_url = 'https://github.com/primakov/precision-medicine-toolbox/archive/refs/tags/0.8.tar.gz',    # I explain this later on
   keywords = ['medical imaging research', 'dicom', 'radiomics', 'statistical analysis', 'features'],   # Keywords that define your package best
   install_requires=[
     'SimpleITK',
